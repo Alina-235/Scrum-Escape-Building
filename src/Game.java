36a3e9fe499a827150abd17a90c3d169c77f90da
rtirdad <94053847+rtirdad@users.noreply.github.com>
2025-05-09 13:42:55 +0200

    speler class verwijderd, game class updaten

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,26 @@
+import java.util.ArrayList;
+
 public class Game {
     private Speler speler;
     private boolean gameOver = false;
+    public static ArrayList<Speler> speler = new ArrayList<>();
+    //private int currentKamer;
 
     public Game(Speler speler) {
         this.speler = speler;
     }
 
-    public void startSpel() {
-        System.out.println("Je staat nu in kamer " + speler.getHuidigeKamer());
+    public void startGame() {
+        System.out.println("Je staat nu in kamer " + speler.currentKamer());
     }
 
     public void toonStatus() {
         System.out.println("Speler: " + speler.getNaam());
         System.out.println("Levens: " + speler.getLives());
-        System.out.println("Huidige kamer: " + speler.getHuidigeKamer());
+        System.out.println("Huidige kamer: " + spelercurrentKamer());
     }
 
-    public void stopSpel() {
+    public void stopGame() {
         System.out.println("Bedankt voor het spelen!");
         gameOver = true;
     }
