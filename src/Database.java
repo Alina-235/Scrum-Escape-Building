35e9659037c7e31c8b2ee1fe3273c6145fb556d0
Alina-235 <alinagao2005@gmail.com>
2025-05-09 22:12:56 +0200

    database insert/select character aangemaakt

--- src/Database.java
+++ src/Database.java
@@ -1,11 +1,11 @@
 import java.sql.*;
 
 public class Database {
-    private String url = "jdbc:mysql://localhost:3306/scrum_escape_building";
-    private String username = "root";
-    private String password = "AGao2005.";
+    private static String url = "jdbc:mysql://localhost:3306/scrum_escape_building";
+    private static String username = "root";
+    private static String password = "AGao2005.";
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         try {
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
@@ -17,4 +17,41 @@ public class Database {
 
 class databaseInsert extends Database{
 
+    public void InsertCharacter(String naam) {
+        try (Connection conn = getConnection()) {
+            String sql = "INSERT INTO gamecharacter (naam) VALUES (?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, naam);
+
+            int rowsInserted = stmt.executeUpdate();
+            System.out.println("Karakter " + naam + " is toegevoegd.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class databaseSelect extends Database {
+
+    public void SelectCharacter(int nummer) {
+        try (Connection connection = getConnection()) {
+            String sql = "SELECT * FROM gamecharacter WHERE nummer = ?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, nummer);
+
+            ResultSet result = stmt.executeQuery();
+
+            if (result.next()) {
+                System.out.println("U speelt nu als karakter " + nummer);
+                System.out.println();
+            }
+            else{
+                System.out.println("Deze karakter is niet gevonden.");
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
-    static Speler speler = new Speler();  // New player structure
-    static Menu menu = new Menu();
-    static Game game = new Game(speler);
-    Database database = new Database();
 
     public static void main(String[] args) {
-        database.getConnection();
+        Database.getConnection();
         System.out.println("Welkom bij Scrum Escape Building!");
 
-        if (!speler.isIngelogd()) {
-            menu.MenuInloggen(speler);
+        if (Speler.isIngelogd()) {
+            Menu.MenuInloggen();
         }
 
-        while (!game.GameOver()) {
-            menu.MainMenu(game);
+        while (Game.GameOver()) {
+            Menu.MainMenu();
         }
     }
 }
