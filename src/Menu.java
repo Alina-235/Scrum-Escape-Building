652ca04f28d9819fbaa86f7da1562703a426b8fb
Lola236 <23069015@student.hhs.nl>
2025-05-13 11:00:47 +0200

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,18 +1,11 @@
-public class Menu {
-    Menukeuze keuze = new Menukeuze();
+class Menu {
 
-    public void MenuInloggen(Speler speler) {
-        System.out.println("Login Menu:");
-        System.out.println("1. Inloggen");
-        System.out.println("2. Afsluiten");
-        keuze.MenuInloggenKeuze(speler);
-    }
-
-    public void Hoofdmenu(Game game) {
-        System.out.println("Hoofdmenu:");
-        System.out.println("1. Start spel");
-        System.out.println("2. Toon status");
-        System.out.println("3. Stoppen");
-        keuze.HoofdmenuKeuze(game);
+    public void MainMenu(){
+        System.out.println("1. Start Game");
+        System.out.println("2. Log in");
+        System.out.println("3. Exit");
+        System.out.println();
+        System.out.print("Maak uw keuze: ");
+        Menukeuze.mainmenukeuze();
     }
 }
