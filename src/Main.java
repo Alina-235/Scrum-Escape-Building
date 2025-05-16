d19331404a789065fbd625fd86006692128ddf7c
Alina-235 <alinagao2005@gmail.com>
2025-05-16 12:56:55 +0200

    merge conflict fixed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Game game = new Game (new Speler());
+    static Speler speler = new Speler();
+    static Menu menu = new Menu();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Database.getConnection();
+        System.out.println("Welkom bij Scrum Escape Buidling!");
+
+        menu.MainMenu();
+
+        while (!game.GameOver()) {
+            game.startGame();
         }
     }
 }
