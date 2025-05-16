3cdcd5973c961ebc5af1bf7b32a19e3567fff91c
Alina-235 <alinagao2005@gmail.com>
2025-05-16 12:55:33 +0200

    conflicts fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Game game = new Game (new Speler());
+    static Speler speler = new Speler();
+    static Menu menu = new Menu();
+
     public static void main(String[] args) {
         Database.getConnection();
         System.out.println("Welkom bij Scrum Escape Buidling!");
