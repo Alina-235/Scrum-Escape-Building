a8e018ec71c1f17038cd821adce97a106d8522ad
Alina-235 <alinagao2005@gmail.com>
2025-06-03 10:47:23 +0200

    while-loop in menu weg

--- src/Menu.java
+++ src/Menu.java
@@ -2,14 +2,12 @@ class Menu {
     static Menukeuze keuze = new Menukeuze();
 
     public void mainMenu() {
-        while (true) {
             System.out.println("1. Start Game");
             System.out.println("2. Log in");
             System.out.println("3. Exit");
             System.out.println();
             System.out.print("Maak uw keuze: ");
             keuze.menu();
-        }
     }
 }
 
