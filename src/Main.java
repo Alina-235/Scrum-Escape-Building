b9e8c1b5d353447b8da8b748ced0a5b775e4fdac
Alina-235 <alinagao2005@gmail.com>
2025-06-05 10:49:48 +0200

    nvm

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static Speler speler = new Speler("", 0);
-    static Game game = new Game (speler);
     static Menu menu = new Menu();
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Database.getConnection();
