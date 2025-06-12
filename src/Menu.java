0f5e90d79ab4589acc39b27bd64758b23748d9da
Lola236 <23069015@student.hhs.nl>
2025-06-12 11:35:01 +0200

    menu is veranderd hihi

--- src/Menu.java
+++ src/Menu.java
@@ -2,11 +2,6 @@ class Menu {
     static Menukeuze keuze = new Menukeuze();
 
     public void mainMenu() {
-            System.out.println("1. Start Game");
-            System.out.println("2. Log in");
-            System.out.println("3. Exit");
-            System.out.println();
-            System.out.print("Maak uw keuze: ");
             keuze.menu();
     }
 }
